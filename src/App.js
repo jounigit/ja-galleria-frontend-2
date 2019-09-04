1aeaf05419f1538f6b6d7384040c25ad8eb67393
Jouni <airaksinen.jouni@gmail.com>
2019-09-04 18:45:40 +0300

    alustusta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [Pictures, setPictures] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:8000/api/pictures')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPictures(response.data)
+      })
+  }, [])
+
+  console.log('Render', Pictures.length, 'products')
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+        <h1>Muistiinpanot</h1>
+
+        
+
     </div>
   );
 }
