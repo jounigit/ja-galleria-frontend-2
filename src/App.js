81a2c1ffbad46cde34914dde8cf3f78c523d90c7
Jouni <airaksinen.jouni@gmail.com>
2019-09-10 15:27:05 +0300

    react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import PictureList from './components/picture/PictureList'
-import './App.css'
+import { BrowserRouter as Router,Route, Link } from 'react-router-dom'
 
+import './App.css'
+import { PictureList } from './components/Picture'
+import { Home } from './components/Home'
 const App = () => {
+  const padding = { padding: 5 }
+
   return (
     <div>
-      <h1>
-        Kuvagalleria
-      </h1>
-
-      <PictureList />
+      <Router>
+        <div>
+          <div>
+            <Link style={padding} to="/">home</Link>
+            <Link style={padding} to="/pictures">pictures</Link>
+          </div>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/pictures" render={() => <PictureList />} />
+        </div>
+      </Router>
     </div>
   )
 }
