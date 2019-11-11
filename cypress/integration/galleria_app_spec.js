94a908c85a6d39c2c4c74b348618ddf32fc4d510
Jouni <airaksinen.jouni@gmail.com>
2019-11-11 20:12:47 +0200

    cypress testing

---
+++ cypress/integration/galleria_app_spec.js
@@ -0,0 +1,27 @@
+describe('Galleria ', function() {
+  beforeEach(function() {
+    cy.visit('http://localhost:3000')
+  })
+  
+  it('front page can be opened', function() {
+    cy.contains('Kuvagalleria')
+  })
+
+  it('pictures page can be opened', function() {
+    cy.contains('pictures')
+	.click()
+    cy.contains('Kuvat')
+  })
+
+  it('albums page can be opened', function() {
+    cy.contains('albums')
+	.click()
+    cy.contains('Albumit')
+  })
+
+  it('categories page can be opened', function() {
+    cy.contains('categories')
+	.click()
+    cy.contains('Kategoriat')
+  })
+})
