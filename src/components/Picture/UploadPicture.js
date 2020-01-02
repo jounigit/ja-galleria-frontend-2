e1ad31b3eb5fee6c4a752fe4da67e2a95d5533df
Jouni <airaksinen.jouni@gmail.com>
2020-01-02 07:55:48 +0200

    master

--- src/components/Picture/UploadPicture.js
+++ src/components/Picture/UploadPicture.js
@@ -111,7 +111,7 @@ const UploadPicture = ({ ...props }) => {
       </Container>
     )
   }
-  const buttonProps = <Button content='valitse' type="button" />
+  // const buttonProps = <Button content='valitse' type="button" />
   // :::::::::::::::::::::::::::::::::::: //
   return(
     <Container>
