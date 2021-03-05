af1cea7b41ecde826023cad51ae35ae5340044d2
Jouni Airaksinen <airaksinen.jouni@gmail.com>
2021-03-05 14:40:44 +0200

    deploy master

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,7 @@ module.exports = {
   },
   'extends': [
     'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:jest/recommended'
+    'plugin:react/recommended'
   ],
   'parserOptions': {
     'ecmaFeatures': {
